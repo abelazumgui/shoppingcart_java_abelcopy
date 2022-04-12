296ad2b9fb3d0267bf857f1f108e74d4032533b0
PtakDev <79053918+PtakDev@users.noreply.github.com>
2022-04-12 11:46:02 +0200

    first commit

---
+++ src/main/java/com/example/demo/ShoopingcartApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoopingcartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoopingcartApplication.class, args);
+	}
+
+}
