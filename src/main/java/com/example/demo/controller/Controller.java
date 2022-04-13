08145ea152a0ffc89598dc7ca588d3607e7730a4
PtakDev <79053918+PtakDev@users.noreply.github.com>
2022-04-13 12:25:02 +0200

    Controllador, Service, Inetrafce para Clienet

---
+++ src/main/java/com/example/demo/controller/Controller.java
@@ -0,0 +1,25 @@
+package com.example.demo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.interfaces.IClienteService;
+import com.example.demo.model.Cliente;
+
+@RestController
+@RequestMapping("/clientes")
+public class Controller {
+	
+	@Autowired
+	IClienteService clienteService;
+
+	@GetMapping("/list_all")
+	public List<Cliente> listClientes() {
+		return clienteService.listado_clientes();
+	}
+
+}
